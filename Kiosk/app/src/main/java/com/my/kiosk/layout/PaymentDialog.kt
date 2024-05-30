2c22e6072948cbce1eb76c4c4f53fb12a9b5d7f9
it215 <example@example.com>
2024-05-30 16:07:47 +0900

    .

--- Kiosk/app/src/main/java/com/my/kiosk/layout/PaymentDialog.kt
+++ Kiosk/app/src/main/java/com/my/kiosk/layout/PaymentDialog.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +14,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -39,10 +39,13 @@ fun PaymentDialog(
             shoppingCart.value.forEach {data ->
                 totalPrice += (data.value.price.value + data.value.optPrice) * data.value.quantity.value
             }
-            TextField(
-                value = text,
-                onValueChange = {
-                    text = it
+            TextField (
+                value = text.value,
+                onValueChange = {it:String ->
+                    text.value = it
+                },
+                label = {
+                    Text("결제 금액")
                 }
             ){
 
